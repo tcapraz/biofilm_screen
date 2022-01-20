738cb06b5b2ab570c00d98e88e11f2cb04df5832
capraz <tuemayc@hotmail.com>
2022-01-20 08:44:55 +0100

    add qc to cropping script

--- cropping/detect_circles_single_windows.py
+++ cropping/detect_circles_single_windows.py
@@ -5,13 +5,14 @@ import cv2
 import matplotlib.pyplot as plt
 import os
 from PIL import Image
-files = os.listdir("annotated_in")
+files = os.listdir("in")
 
 notfound =0
+ambiguous=0
 for f in files:
     xstart = 320
     ystart = 566
-    image = cv2.imread(os.path.join("annotated_in",f))
+    image = cv2.imread(os.path.join("in",f))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     output = image[320:3120,566:4694].copy()
     imagesmall = image[320:3120,566:4694].copy()
@@ -35,14 +36,26 @@ for f in files:
                 cx = int(circles[0,0,1]) +xstart + x
                 cy = int(circles[0,0,0]) + ystart + y
                 margin= 80
-                crop = image[cx-margin:cx+margin,cy-margin:cy+margin] 
+                crop = image[cx-margin:cx+margin,cy-margin:cy+margin]
+                cropgray =  cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+                ax0 = np.var(cropgray, axis=0)
+                ax1 =np.var(cropgray, axis=1)
+                edge_var = np.array([np.sum(ax0[0:5]), np.sum(ax0[-5:]), np.sum(ax1[0:5]), np.sum(ax1[-5:])])
+
+
+
                 im = Image.fromarray(crop).convert('RGB')
                 name,_ = f.split(".")
-                if circles.shape[1] > 1:
+                # if circles.shape[1] > 1:
+                #     fname = name + "_x" + str(j+1) + "_y" + str(i+1) +"_ambiguous.jpg"
+                if any(edge_var > 100):
                     fname = name + "_x" + str(j+1) + "_y" + str(i+1) +"_ambiguous.jpg"
+                    ambiguous +=1
+                    im.save(os.path.join("out_new2/bad",fname))
+
                 else:
                     fname = name + "_x" + str(j+1) + "_y" + str(i+1) +".jpg"
-                im.save(os.path.join("annotated_out",fname))
+                    im.save(os.path.join("out_new2/clean",fname))
 
             else:
                 notfound +=1
